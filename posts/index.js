a6ecf87f2e6d8c9fe6e0c9cf53080993516eb5df
Radostin Marinov <r.marinov045@gmail.com>
2022-05-23 13:44:11 +0300

    Adding initial posts server setup

---
+++ posts/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { randomBytes } = require('crypto')
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(4000, () => {
+    console.log('Posts server listening on port 4000...');
+});
+
+const posts = {}
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    posts[id] = {
+        id, title
+    };
+
+    res.status(201).send(posts[id]);
+});
+

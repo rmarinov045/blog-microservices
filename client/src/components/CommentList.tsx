226abe9488761f2fa0e75b577d6777c18716fbdb
Radostin Marinov <r.marinov045@gmail.com>
2022-05-25 23:07:29 +0300

    Adding conditional rendering based on status for comments

--- client/src/components/CommentList.tsx
+++ client/src/components/CommentList.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
 export interface Comment {
-        id: string,
-        content: string
+    id: string,
+    content: string,
+    status: 'approved' | 'pending' | 'rejected',
 };
 
 function CommentList({ comments }: { comments: Comment[] }) {
 
     const renderedComments = comments.map(comment => {
+        let content: string = comment.content;
+
+        if (comment.status === 'pending') content = 'This comment is awaiting moderation';
+        if (comment.status === 'rejected') content = 'This comment has been rejected';
+
         return (
-            <li key={comment.id}>{comment.content}</li>
+            <li key={comment.id}>{content}</li>
         );
     });
 

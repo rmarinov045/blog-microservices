e5713237066a4c9819dee0bee4aad325c469289c
Radostin Marinov <r.marinov045@gmail.com>
2022-05-25 23:34:26 +0300

    Adding event syncing between event bus and query service

--- query/index.js
+++ query/index.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Query server listening on port 4002....');
+
+    const response = await axios.get('http://localhost:4005/events').catch(error => {
+        console.log(error);
+    });
+
+    for (const event of response.data) {
+        console.log('Processing event:', event.type);
+
+        handleEvent(event.type, event.data);
+    }
 });
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
 
@@ -42,8 +47,16 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+}
 
-    console.log(posts);
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
 
     res.send({});
 });
